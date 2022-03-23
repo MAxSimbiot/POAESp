43e580cc59ac65b82bdcab014a8a401b9852057c
Max <maxsimbiot@gmail.com>
2022-03-23 19:42:01 +0200

    First task

--- src/main/java/com/sages/SpringBootApplication.java
+++ src/main/java/com/sages/SpringBootApplication.java
@@ -5,6 +5,6 @@ import org.springframework.boot.SpringApplication;
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringApplication.class,args);
+         SpringApplication.run(SpringApplication.class,args);
     }
 }
